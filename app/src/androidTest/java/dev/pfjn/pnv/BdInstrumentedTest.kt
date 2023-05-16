a5ae2bd9b801da97180f25c3c3db18e2c88a2035
PfJn <88943477+pfjndev@users.noreply.github.com>
2023-05-16 15:47:23 +0100

    Implemented getWritableDatabase() function, it returns a database with write permissions.
    Implemented consegueInserirDoencas() function, it tries to insert a disease in the database by calling insereDoenca() function.
    
    Implemented insereDoenca() function, it inserts a disease in the database and checks if its ID is != than the default value.
    
    Implemented consegueInserirVacina() function, it tries to insert a vaccine in the database by calling insereVacina() function.
    
    Implemented insereVacina() function, it inserts a vaccine in the database and checks if its ID is != than the default value.
    
    Implemented consegueLerDoencas() test.
    It requests a writable database, creates testing diseases and inserts them on the database. Then, it creates a cursor to retrieve data from the database.
    Finally it checks if the data inserted and the data retrieved from the cursor are the same.
    
    Implemented consegueLerVacinas() test.
    It requests a writable database, creates testing vaccines and inserts them on the database. Then, it creates a cursor to retrieve data from the database.
    Finally it checks if the data inserted and the data retrieved from the cursor are the same.

--- app/src/androidTest/java/dev/pfjn/pnv/BdInstrumentedTest.kt
+++ app/src/androidTest/java/dev/pfjn/pnv/BdInstrumentedTest.kt
@@ -1,6 +1,8 @@
 package dev.pfjn.pnv
 
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -28,6 +30,121 @@ class BdInstrumentedTest {
         val openHelper = BdPnvOpenHelper(getAppContext())
         val bd = openHelper.readableDatabase
         assert(bd.isOpen)
+    }
+    private fun getWritableDatabase() : SQLiteDatabase {
+        val openHelper = BdPnvOpenHelper(getAppContext())
+        return openHelper.writableDatabase
+    }
+
+    fun consegueInserirDoencas() {
+        val bd = getWritableDatabase()
+
+        val doenca = Doenca("VHB")
+        insereDoenca(bd, doenca)
+    }
+
+    private fun insereDoenca(bd: SQLiteDatabase, doenca: Doenca) {
+       doenca.id = TabelaDoencas(bd).insere(doenca.toContentValues())
+       assertNotEquals(-1, doenca.id)
+    }
+
+    fun consegueInserirVacinas() {
+        val bd = getWritableDatabase()
+
+        val doenca = Doenca ("VHB")
+        insereDoenca(bd, doenca)
+
+        val vacina1 = Vacina("Vírus Hepatite B", doenca.id, "1 Dose: Nascimento, 2 Dose: 2 Meses, 3 Dose: 6 Meses")
+        insereVacina(bd, vacina1)
+
+        val doenca2 = Doenca("Hib")
+        insereDoenca(bd, doenca2)
+
+        val vacina2 = Vacina("Haemophilus influenzae tipo b", doenca2.id, "1 Dose: 2 Meses, 2 Dose: 4 Meses, 3 Dose: 6 Meses, 4 Dose: 18 Meses")
+        insereVacina(bd, vacina2)
+    }
+
+    private fun insereVacina(bd: SQLiteDatabase, vacina: Vacina) {
+        vacina.id = TabelaVacinas(bd).insere(vacina.toContentValues())
+        assertNotEquals(-1, vacina.id)
+    }
+
+    @Test
+    fun consegueLerDoencas() {
+        val bd = getWritableDatabase()
+
+        val doencaVHB = Doenca("VHB")
+        insereDoenca(bd, doencaVHB)
+
+        val doencaHib = Doenca("Hib")
+        insereDoenca(bd, doencaHib)
+
+        val tabelaDoencas = TabelaDoencas(bd)
+
+        val cursor = tabelaDoencas.consulta(
+            TabelaDoencas.CAMPOS,
+            "${BaseColumns._ID}=?",
+            arrayOf(doencaVHB.id.toString()),
+            null,
+            null,
+            null
+        )
+
+        assert(cursor.moveToNext())
+
+        val doencaBD = Doenca.fromCursor(cursor)
+
+        assertEquals(doencaVHB, doencaBD)
+
+        val cursorTodasDoencas = tabelaDoencas.consulta(
+            TabelaDoencas.CAMPOS,
+            null,
+            null,
+            null,
+            null,
+            TabelaDoencas.CAMPO_DESCRICAO
+        )
+
+        assert(cursorTodasDoencas.count > 1)
+    }
+
+    @Test
+    fun consegueLerVacinas() {
+        val bd = getWritableDatabase()
+
+        val doenca = Doenca("VIP")
+        insereDoenca(bd, doenca)
+
+        val vacina1 = Vacina("Poliomelite", doenca.id,"1 Dose: 2 Meses, 2 Dose: 4 Meses, 3 Dose: 6 Meses, 4 Dose: 18 Meses, 5 Dose: 5 anos")
+        insereVacina(bd, vacina1)
+
+        val tabelaVacinas = TabelaVacinas(bd)
+
+        val cursor = tabelaVacinas.consulta(
+            TabelaVacinas.CAMPOS,
+            "${BaseColumns._ID}=?",
+            arrayOf(vacina1.id.toString()),
+            null,
+            null,
+            null
+        )
+
+        assert(cursor.moveToNext())
+
+        val vacinaBD = Vacina.fromCursor(cursor)
+
+        assertEquals(vacina1, vacinaBD)
+
+        val cursorTodasVacinas = tabelaVacinas.consulta(
+            TabelaVacinas.CAMPOS,
+            null,
+            null,
+            null,
+            null,
+            TabelaVacinas.CAMPO_NOME
+        )
+
+        assert(cursorTodasVacinas.count > 1)
     }
     @Test
     fun useAppContext() {
